db077ce0dd3ed7b5f306c0294f447031be5c5624
kaleeswari <kaleeswari@sportstech.de>
2026-02-09 16:33:37 +0530

    deleted .next

--- .next/dev/server/app/api/blueprint/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/blueprint/route.js")
-R.c("server/chunks/7d925_next_8a8c3f89._.js")
-R.c("server/chunks/7d925_openai_df12822c._.js")
-R.c("server/chunks/[root-of-the-server]__57629377._.js")
-R.c("server/chunks/80b94_GitHub_bee_io__next-internal_server_app_api_blueprint_route_actions_486bcd80.js")
-R.m("[project]/Documents/GitHub/bee.io/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Documents/GitHub/bee.io/app/api/blueprint/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/Documents/GitHub/bee.io/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Documents/GitHub/bee.io/app/api/blueprint/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- app/components/whychooseus.tsx
+++ app/components/whychooseus.tsx
@@ -8,7 +8,7 @@ import NavLabel from "./NavLable";
 const Whychooseus = () => {
   return (
     
-    <section className="w-full bg-[#00020F] py-16 md:py-20 flex justify-center text-white">
+    <section className="w-full bg-[#00020F] py-10 md:py-10 flex justify-center text-white">
       
       <div className="relative w-full max-w-[1200px] xl:px-6 lg:px-6 md:px-1 hidden md:block ">
              <div className="mb-16 flex justify-center">
@@ -24,10 +24,8 @@ const Whychooseus = () => {
             {/* ICON CARD */}
             <div className="flex-1 rounded-[32px] border border-white/10 bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505] shadow-[inset_0_1px_1px_rgba(255,255,255,0.15),_0_20px_40px_rgba(0,0,0,0.8)] xl:p-8 lg:p-8 md:p-4 sm:p-4 p-4 flex items-center justify-center">
              
-    <img
-                  src="/assets/images/dark-btn.png"
-                  alt="icon"
-                />  
+<p className="text-[#43C0A3] text-center xl:text-2xl lg:text-2xl md:text-sm font-bold">
+Pioneer blockchain Engineering</p>
  
  
             </div>
@@ -43,10 +41,7 @@ const Whychooseus = () => {
 
             {/* PROJECT CARD (bigger weight) */}
             <div className="flex-[2] rounded-[32px] border border-white/10 bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505] shadow-[inset_0_1px_1px_rgba(255,255,255,0.15),_0_20px_40px_rgba(0,0,0,0.8)] xl:p-8 lg:p-8 md:p-6 sm:p-4 p-4 flex flex-col justify-between">
-              <h3 className="xl:text-2xl xl:text-2xl md:text-sm font-bold text-emerald-50">
-                Project Ideation & Planning
-              </h3>
-
+             
               {/* Decorative floating elements */}
               <div className="relative xl:h-72 lg:h-72 md:h-unset">
               
@@ -112,14 +107,14 @@ const Whychooseus = () => {
               <img
                 src="/assets/images/small-avatar-icon.png"
                 alt="avatar"
-                className="w-auto h-auto rounded-full"
+                className="w-30  rounded-full"
               />
             </div>
 
             {/* MAINTENANCE CARD */}
-            <div className="flex-1 rounded-[32px] border border-white/10 bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505] shadow-[inset_0_1px_1px_rgba(255,255,255,0.15),_0_20px_40px_rgba(0,0,0,0.8)] xl:p-8 lg:p-8 md:p-4 sm:p-4 flex items-center justify-center">
+            <div className="flex-1 text-black rounded-[32px] font-bold border border-white/10 bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505] shadow-[inset_0_1px_1px_rgba(255,255,255,0.15),_0_20px_40px_rgba(0,0,0,0.8)] xl:p-8 lg:p-8 md:p-4 sm:p-4 flex items-center justify-center">
               <button className="xl:text-2xl lg:text-2xl md:text-[10px] font-medium px-4 py-2 bg-gradient-to-r from-[#7ee8ec] to-[#50a7b0] rounded-full">
-                Maintenance & Upgrades
+                Long-Term <br/> Partnership
               </button>
             </div>
           </div>
@@ -150,11 +145,8 @@ const Whychooseus = () => {
               bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505]
               shadow-[inset_0_1px_1px_rgba(255,255,255,0.12),_0_16px_30px_rgba(0,0,0,0.7)]
               p-3 md:p-5 flex items-center justify-center">
-              <img
-                src="/assets/images/dark-btn.png"
-                alt="icon"
-                className="w-16 md:w-20"
-              />
+             <p className="text-[#43C0A3] text-center xl:text-2xl lg:text-2xl md:text-sm font-bold">
+              Pioneer blockchain Engineering</p>
             </div>
 
             {/* EVO AI CARD */}
@@ -178,11 +170,8 @@ const Whychooseus = () => {
               bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505]
               shadow-[inset_0_1px_1px_rgba(255,255,255,0.12),_0_16px_30px_rgba(0,0,0,0.7)]
               p-3 md:p-5 flex flex-col justify-between">
-              <h3 className="text-base md:text-xl font-bold text-emerald-50">
-                Project Ideation & Planning
-              </h3>
-
-              <div className="relative h-28 md:h-40">
+             
+              <div className="relative h-40 md:h-40">
                 <Image
                   src="/assets/images/floting-text.png"
                   alt="Idea"
@@ -199,7 +188,7 @@ const Whychooseus = () => {
               className="
                 relative
                 w-full
-                h-[540px]
+                h-[450px]
                 sm:h-[520px]
                 md:w-[520px]
               "
@@ -237,7 +226,7 @@ const Whychooseus = () => {
             {/* DESIGN CARD */}
             <div className="flex-[1.8] rounded-2xl md:rounded-[28px] border border-white/10
               bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505]
-              shadow-[inset_0_1px_1px_rgba(255,255,255,0.12),_0_16px_30px_rgba(0,0,0,0.7)]
+              shadow-[inset_0_1px_1px_rgba(255,255,255,0.12),_0_16pxexec_30px_rgba(0,0,0,0.7)]
               p-3 md:p-5 flex flex-col justify-between">
               <Image
                 src="/assets/images/small-logo.svg"
@@ -273,18 +262,18 @@ const Whychooseus = () => {
               <img
                 src="/assets/images/small-avatar-icon.png"
                 alt="avatar"
-                className="w-10 rounded-full"
+                className="w-24 rounded-full"
               />
             </div>
 
             {/* MAINTENANCE CARD */}
-            <div className="flex-[1] rounded-2xl md:rounded-[28px] border border-white/10
+            <div className="flex-[1] text-black rounded-2xl md:rounded-[28px] border border-white/10
               bg-gradient-to-br from-[#2A2A2A] via-[#151515] to-[#050505]
               shadow-[inset_0_1px_1px_rgba(255,255,255,0.12),_0_16px_30px_rgba(0,0,0,0.7)]
               p-3 md:p-5 flex items-center justify-center">
-              <button className="text-xs md:text-lg px-4 py-1.5 rounded-full font-medium
+              <button className="text-[14px]  md:text-lg px-4 py-1.5 rounded-full font-medium
                 bg-gradient-to-r from-[#7ee8ec] to-[#50a7b0]">
-                Maintenance & Upgrades
+                Long-Term <br/> Partnership
               </button>
             </div>
           </div>
